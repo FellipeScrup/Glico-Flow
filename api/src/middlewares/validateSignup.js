de8a13cdcbdd5d341cccbc2c6111e2347cc09e4e
FellipeScrup <fellipescruph@gmail.com>
2024-11-08 17:10:28 -0300

    Remoção da pasta api

--- api/src/middlewares/validateSignup.js
+++
@@ -1,8 +0,0 @@
-// middlewares/validateSignup.js
-module.exports = (req, res, next) => {
-    const { email, password, termsAccepted } = req.body;
-    if (!email || !password || !termsAccepted) {
-        return res.status(400).json({ message: 'Missing required fields' });
-    }
-    next();
-};
